ac8125552291485df440a8739878ea793b8470d3
Aji3na <120646382+Aji3na@users.noreply.github.com>
2022-12-26 14:57:19 +0530

    Update calculator.js
    Button added with iteration

--- src/calculator.js
+++ src/calculator.js
@@ -1,114 +1,132 @@
-import StyledButton from "./components/buttons/operator";
-import ButtonDiv from "./components/buttons/BtnDiv"
-import { SectionStyle } from "./components/Section";
-import { StyledDisplay } from "./components/buttons/display";
-import React from "react";
- const Title = (props) => {
+import StyledButton from './components/buttons/operator';
+import ButtonDiv from './components/buttons/BtnDiv';
+import { SectionStyle } from './components/Section';
+import { StyledDisplay } from './components/buttons/display';
+import React from 'react';
+const Title = (props) => {
   return (
     <div className="calculator-title">
       <h1>{props.value} </h1>
     </div>
-  )
-}
- export const Display = (props) => {
+  );
+};
+export const Display = (props) => {
   return (
-    <input value ={props.value} type="text" className='screen' placeholder='0' readOnly ></input>
-  )
-}
- const ButtonValue = (props) => {
+    <input
+      value={props.value}
+      type="text"
+      className="screen"
+      placeholder="0"
+      readOnly
+    ></input>
+  );
+};
+const ButtonValue = (props) => {
   return (
-    <div><input type="button" value={props.BtnValue} onClick = {props.handleClick}/>
+    <div>
+      <input type="button" value={props.BtnValue} onClick={props.handleClick} />
     </div>
-  )
-}
+  );
+};
 class CalculatorApp extends React.Component {
   constructor() {
     super();
     this.state = {
       typing: '',
-      result: ''
-    }
+      result: '',
+    };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(event) {
     //console.log('The link has been  clicked.');
+    var Typing = this.state.typing;
     const value = event.target.value;
     console.log(event.target.value);
     switch (value) {
       case '=': {
-        if (this.state.typing !== '') {
+        if (Typing !== '') {
           var finalResult = '';
           try {
-            finalResult = eval(this.state.typing);
+            finalResult = eval(Typing);
             console.log(finalResult);
           } catch (error) {
-            this.setState({ result: "Syntax Error"});
-            console.log("Error");
+            this.setState({ result: 'Syntax Error' });
+            console.log('Error');
           }
-          if (finalResult === undefined) 
-            this.setState({ result: "Syntax Error" });
-          else
-          this.setState({result: finalResult, typing: finalResult});
+          if (finalResult === undefined)
+            this.setState({ result: 'Syntax Error' });
+          else this.setState({ result: finalResult, typing: finalResult });
           break;
         }
-        
       }
-      case 'Clear':{
-        this.setState({result:'' ,typing: ''});
+      case 'Clear': {
+        this.setState({ result: '', typing: '' });
 
         break;
       }
-      case 'Del':{
-        var str = this.state.typing;
-        str = str.slice(0,-1);
-        this.setState({typing: str});
+      case 'Del': {
+        var str = Typing;
+        str = str.slice(0, -1);
+        this.setState({ typing: str });
         break;
       }
-      default:{
-        this.setState({typing: this.state.typing += value});
+      default: {
+        this.setState({ typing: (Typing += value) });
         break;
       }
     }
   }
   render() {
+    var Typings = this.state.typing;
+    let row1 = ['Clear', 'Del', '%', '/'];
+    let row2 = ['7', '8', '9', '*'];
+    let row3 = ['4', '5', '6', '-'];
+    let row4 = ['1', '2', '3', '+'];
+    let row5 = ['0', '.', '='];
     return (
       <SectionStyle>
         <Title value="My calculator"></Title>
         <div className="calculator-container">
           <StyledDisplay>
-          <Display value = { this.state.typing}></Display>
-      {/* <Display value = {this.state.result}></Display> */}
+            <Display value={Typings}></Display>
+            {/* <Display value = {this.state.result}></Display> */}
           </StyledDisplay>
-          <div className='button-container'>
-            <ButtonDiv>
-              <StyledButton variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'Clear'}></ButtonValue></StyledButton>
-              <StyledButton variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'Del'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'%'}></ButtonValue></StyledButton>
-              <StyledButton className="button-bg" variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'/'}></ButtonValue></StyledButton>
-            </ButtonDiv>
-            <ButtonDiv>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'7'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'8'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'9'}></ButtonValue></StyledButton>
-              <StyledButton className="button-bg" variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'*'}></ButtonValue></StyledButton>
-            </ButtonDiv>
-            <ButtonDiv>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'4'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'5'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'6'}></ButtonValue></StyledButton>
-              <StyledButton className="button-bg" variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'-'}></ButtonValue></StyledButton>
-            </ButtonDiv>
-            <ButtonDiv>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'1'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'2'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'3'}></ButtonValue></StyledButton>
-              <StyledButton className="button-bg" variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'+'}></ButtonValue></StyledButton>
-            </ButtonDiv>
-            <ButtonDiv>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'0'}></ButtonValue></StyledButton>
-              <StyledButton ><ButtonValue handleClick = {this.handleClick} BtnValue={'.'}></ButtonValue></StyledButton>
-              <StyledButton className="equal" variant="operator"><ButtonValue handleClick = {this.handleClick} BtnValue={'='}></ButtonValue></StyledButton>
-            </ButtonDiv>
+          <div className="button-container">
+            <div class="new-btn">
+              {row1.map((item) => {
+                return (
+                  <ButtonValue handleClick={this.handleClick} BtnValue={item} />
+                );
+              })}
+            </div>
+            <div class="new-btn">
+              {row2.map((item) => {
+                return (
+                  <ButtonValue handleClick={this.handleClick} BtnValue={item} />
+                );
+              })}
+            </div>
+            <div class="new-btn">
+              {row3.map((item) => {
+                return (
+                  <ButtonValue handleClick={this.handleClick} BtnValue={item} />
+                );
+              })}
+            </div>
+            <div class="new-btn">
+              {row4.map((item) => {
+                return (
+                  <ButtonValue handleClick={this.handleClick} BtnValue={item} />
+                );
+              })}
+            </div>
+            <div class="new-btn">
+              {row5.map((item) => {
+                return (
+                  <ButtonValue handleClick={this.handleClick} BtnValue={item} />
+                );
+              })}
+            </div>
           </div>
         </div>
       </SectionStyle>
